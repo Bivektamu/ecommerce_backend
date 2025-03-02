846e30323fe165c745c09de14f778d744aef9ab6
Bivek Gurung <bivek.tamu@gmail.com>
2025-03-02 12:54:52 +1100

    Mutation added to add review

--- schema/resolvers/review.resolver.ts
+++ schema/resolvers/review.resolver.ts
@@ -1,15 +1,48 @@
 import { Query } from "mongoose";
 import Review from "../../dataLayer/schema/Review";
-import { ReviewType } from "../../typeDefs";
+import { ReviewType, User } from "../../typeDefs";
+import verifyUser from "../../utilities/verifyUser";
 
 const reviewResolver = {
     Query: {
-        reviewsByProductId: async(parent: any, args: any) => {
+        reviewsByProductId: async (parent: any, args: any) => {
             const productId = args.id
-            const reviews = await Review.find({productId })
+            const reviews = await Review.find({ productId })
             console.log(reviews);
-            
+
             return reviews
+        }
+    },
+    Mutation: {
+        createReview: async (parentparent: any, args: any, context: any) => {
+
+            try {
+
+                if (!context.token) {
+                    throw new Error('Not Authenticated')
+                }
+
+                const user = verifyUser(context.token)
+
+                if(!user || user.userRole !== User.CUSTOMER) {
+                    throw new Error('Not Authenticated')
+                }
+
+                const {id, stars, productId, customerId, review} = args.input
+
+                const newReview = new Review({
+                    id,
+                    stars,
+                    productId,
+                    customerId,
+                    review
+                })
+
+                return await newReview.save()
+            } catch (error) {
+                throw error
+            }
+
         }
     }
 }
--- schema/typeDefs/review.typeDef.ts
+++ schema/typeDefs/review.typeDef.ts
@@ -1,15 +1,26 @@
 import gql from "graphql-tag"
 const reviewTypeDef = gql`
     type Review {
-        id: ID,
-        customerId: ID,
-        productId: ID,
-        stars: Int,
-        review: String
+        id: ID!,
+        customerId: ID!,
+        productId: ID!,
+        stars: Int!,
+        review: String!
     }
     
     type Query {
         reviewsByProductId(id: ID): [Review]
+    }
+
+    input CreateReview {
+        id: ID!,
+        customerId: ID!,
+        productId: ID!,
+        stars: Int!,
+        review: String!
+    }
+    type Mutation {
+        createReview(input:CreateReview):Review
     }
 `
 
